5a7369ed0a0037b5903386639cb005d6e84b1e07
Michelle Tessier <michelletessier@michelles-macbook-pro.local>
2017-01-25 14:02:37 -0700

    Initial Commit

---
+++ ThirdPartyKeyboardShiftingDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThirdPartyKeyboardShiftingDemo
+//
+//  Created by Michelle Tessier on 1/25/17.
+//  Copyright © 2017 Michelle Tessier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
